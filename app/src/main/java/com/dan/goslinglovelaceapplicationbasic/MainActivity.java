08b1057e9fd69da450c694ec07ef34db26fa73b4
Zoy0506 <2736048685@qq.com>
2018-03-25 17:35:22 +0800

    finish the basic

---
+++ app/src/main/java/com/dan/goslinglovelaceapplicationbasic/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dan.goslinglovelaceapplicationbasic;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
